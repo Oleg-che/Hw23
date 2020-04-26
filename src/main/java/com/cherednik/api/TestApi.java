37f141b28cc395f3de066180d94b5c97d25ae33d
Gerzonianzes <ol.ccherednik@gmail.com>
2020-04-26 23:20:37 +0300

    commit init

---
+++ src/main/java/com/cherednik/api/TestApi.java
@@ -0,0 +1,31 @@
+package com.cherednik.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class TestApi {
+
+    @GET
+    public Response test() {
+        User user = new User("Alex", 22);
+        Gson gson = new GsonBuilder().create();
+        return Response.status(Response.Status.OK)
+                .entity(gson.toJson(user))
+                .build();
+    }
+
+    private static class User {
+        private String name;
+        private int age;
+
+        User(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
